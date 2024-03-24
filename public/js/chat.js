46e739ed6fb4da3868664488c84ae17f0149b2ce
programmersahil <udarsahil0@gmail.com>
2024-03-24 17:26:52 +0530

    basic chat functionality but there are some errors

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,19 +1,36 @@
 const PORT = window.location.port;
 const socket = io.connect(`http://localhost:${PORT}`);
 
+
 window.onload = () => {
-    connPara = document.getElementById("conn");
+  connPara = document.getElementById("conn");
+  const chatForm = document.getElementById('chat-form');
+  const messageInput = document.getElementById('message-input');
+  const messageContainer = document.getElementById('message-container');
 
-    socket.on('userConn', message => {
-        connPara.style.visibility = "visible";
-        connPara.innerHTML = message;
-        setTimeout(() => {
-            connPara.style.visibility = "hidden";
-        }, 3000)
-});
+  chatForm.addEventListener('submit',(e) => {
+    e.preventDefault();
+    console.log(1);
 
-socket.on('message', message => {
-    console.log(message);
-});
+    const message = messageInput.value;
+
+    socket.emit('emitMessage', {message, username, room})
+    messageInput.value = '';
+  })
+
+  socket.on("userConn", (message) => {
+    connPara.style.visibility = "visible";
+    connPara.innerHTML = message;
+    setTimeout(() => {
+      connPara.style.visibility = "hidden";
+    }, 3000);
+  });
+
+  socket.on("message", (data) => {
+    console.log(data.message);
+    const messageElement = document.createElement('div');
+    messageElement.textContent = `${data.username}: ${data.message}`;
+
+    messageContainer.appendChild(messageElement);
+  });
 };
-
